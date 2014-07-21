be08957ee9642d68791f1cc5181ee3e46d2aa5ce
bgrove1974 <bgrove1974@gmail.com>
2014-07-21 00:47:22 -0700

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-VoterAppApi::Application.routes.draw do 
+VoterAppApi::Application.routes.draw do
+ 
+end
+### Original file below ###
 # Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -54,4 +57,4 @@ VoterAppApi::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+# end
